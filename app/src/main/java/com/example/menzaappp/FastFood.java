d6ffc587a4e015e674d030d654a380e81f86a069
HRyohni <matosevic.leo@gmail.com>
2022-10-05 18:57:38 +0200

    update

--- app/src/main/java/com/example/menzaappp/FastFood.java
+++ app/src/main/java/com/example/menzaappp/FastFood.java
@@ -1,8 +1,13 @@
 package com.example.menzaappp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 public class FastFood extends AppCompatActivity {
@@ -18,5 +23,43 @@ public class FastFood extends AppCompatActivity {
         listView= (ListView) findViewById(R.id.customListView);
         CustomBaseAdapter customBaseAdapter = new CustomBaseAdapter(getApplicationContext(),nazivjela,cijenajela,sadrzajjela);
         listView.setAdapter(customBaseAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId())
+        {
+            case R.id.restoran:
+                intent=new Intent(FastFood.this,MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.fastfood:
+                intent=new Intent(FastFood.this,FastFood.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.about:
+                intent=new Intent(FastFood.this,MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.pizzeria:
+                intent=new Intent(FastFood.this,MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.pitanja:
+                intent=new Intent(FastFood.this,MainActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/menzaappp/MainActivity.java
+++ app/src/main/java/com/example/menzaappp/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.menzaappp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +27,48 @@ public class MainActivity extends AppCompatActivity {
     String nazivjela [] = {"kurac","kurac","kurac","kurac"};
     String cijenajela[]= {"pička","pička","pička","pička"};
     String sadrzajjela[]= {"sisa","sisa","sisa","Pšenično brašno,jaja i mlijeko"};
+    TextView Dan;
     ListView listView;
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId())
+        {
+            case R.id.restoran:
+                 intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.fastfood:
+                 intent=new Intent(MainActivity.this,FastFood.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.about:
+                 intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.pizzeria:
+                intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.pitanja:
+                intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +77,13 @@ public class MainActivity extends AppCompatActivity {
         listView= (ListView) findViewById(R.id.customListView);
         CustomBaseAdapter customBaseAdapter = new CustomBaseAdapter(getApplicationContext(),nazivjela,cijenajela,sadrzajjela);
         listView.setAdapter(customBaseAdapter);
+        Dan = findViewById(R.id.dan);
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+// full name form of the day
+        Dan.setText(new SimpleDateFormat("EEEE", Locale.ENGLISH).format(date.getTime()));
     }
 
 
-    public  void FastFood(View v)
-        {
-            Intent intent=new Intent(MainActivity.this,FastFood.class);
 
-            startActivity(intent);
-        }
 }
